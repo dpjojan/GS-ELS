56fb876cd0edab85e2b67b045e0c8b66c4424219
ambers7 <ambrshen@gmail.com>
2026-02-19 17:27:14 -0500

    Created MutualFund class

--- mutual-fund-backend/src/main/java/com/mutualfund/mutual_fund_backend/MutualFund.java
+++ mutual-fund-backend/src/main/java/com/mutualfund/mutual_fund_backend/MutualFund.java
@@ -4,5 +4,21 @@ package com.mutualfund.mutual_fund_backend;
  * Class for a mutual fund (includes ticker, name of fund)
  */
 public class MutualFund {
-    // TODO: create variables & constructors for mutual fund class
+    private String ticker; //the unique identifiable feature for each mutual fund (needed for Newton API)
+    private String name;
+
+    public MutualFund(String ticker, String name) {
+        this.ticker = ticker;
+        this.name = name;
+    }
+
+    public String getTicker() {
+        return ticker;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
 }
