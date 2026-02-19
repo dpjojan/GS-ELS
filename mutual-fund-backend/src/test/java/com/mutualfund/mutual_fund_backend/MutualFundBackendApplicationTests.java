30642e6260a0a2d50ce09c7ce225d77f1e2878d2
ambers7 <ambrshen@gmail.com>
2026-02-19 16:56:37 -0500

    Added Java SpringBoot template

---
+++ mutual-fund-backend/src/test/java/com/mutualfund/mutual_fund_backend/MutualFundBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mutualfund.mutual_fund_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutualFundBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
