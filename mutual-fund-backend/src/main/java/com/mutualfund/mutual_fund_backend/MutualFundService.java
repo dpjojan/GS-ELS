d3fae0e1e6801c3fa447e89fa504308d32e6b875
ambers7 <ambrshen@gmail.com>
2026-02-26 17:10:16 -0500

    Added placeholder functions in Service for Controller to use
    Co-authored-by: Matthew <kmatthew8363@gmail.com>

--- mutual-fund-backend/src/main/java/com/mutualfund/mutual_fund_backend/MutualFundService.java
+++ mutual-fund-backend/src/main/java/com/mutualfund/mutual_fund_backend/MutualFundService.java
@@ -10,10 +10,22 @@ public class MutualFundService {
     private final NewtonStockBetaClient newtonStockBetaClient = new NewtonStockBetaClient();
 
     //TODO: fxn to calculate future value (given formula)
-
+    public double calculateFutureVal(String ticker, double principal, int years) {
+        double presentValue = 0; //TODO replace
+        double returnRate = 0; // TODO replace
+        double futureValue = Math.pow(presentValue * (1 + returnRate),years);
+        return futureValue; 
+    }
     //TODO: fxn to calculate expected return rate from last year of mutual fund data
+    public double calculateExpectedReturnRate(String ticker, double principal, int years) {
+        
+        double expectedReturnRate = 0; //TODO replace
+        return expectedReturnRate;
+    }
+    
+    
 
-    // fxn to get beta from Newton API
+    // fxn to get beta from Newton API 
     public Optional<Double> getBetaFromNewtonApi(String ticker) {
         return newtonStockBetaClient.getBeta(ticker);
     }
