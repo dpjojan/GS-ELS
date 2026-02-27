2246aea9ce6e66d371bbeb731e38c7371bdf4fe2
s3raphsembrace <kmatthew8363@gmail.com>
2026-02-27 00:46:35 -0500

    Added on to placeholder functions in service

--- mutual-fund-backend/src/main/java/com/mutualfund/mutual_fund_backend/MutualFundRepository.java
+++ mutual-fund-backend/src/main/java/com/mutualfund/mutual_fund_backend/MutualFundRepository.java
@@ -1,7 +1,11 @@
 package com.mutualfund.mutual_fund_backend;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 /**
  * Talks to AWS database
  */
 public class MutualFundRepository {
+    spring.datasource.url = jdcb:posgresl://
 }
--- mutual-fund-backend/src/main/java/com/mutualfund/mutual_fund_backend/MutualFundService.java
+++ mutual-fund-backend/src/main/java/com/mutualfund/mutual_fund_backend/MutualFundService.java
@@ -11,19 +11,28 @@ public class MutualFundService {
 
     //TODO: fxn to calculate future value (given formula)
     public double calculateFutureVal(String ticker, double principal, int years) {
-        double presentValue = 0; //TODO replace
-        double returnRate = 0; // TODO replace
-        double futureValue = Math.pow(presentValue * (1 + returnRate),years);
+        double presentValue = principal;
+        double returnRate = calculateExpectedReturnRate(ticker, principal, years);
+        double futureValue = presentValue * Math.pow((1 + returnRate), years);
         return futureValue; 
     }
     //TODO: fxn to calculate expected return rate from last year of mutual fund data
     public double calculateExpectedReturnRate(String ticker, double principal, int years) {
-        
         double expectedReturnRate = 0; //TODO replace
+        Optional<Double> beta = getBetaFromNewtonApi(ticker);
+        double riskFreeRate = calculateRiskFreeRate();
+        double marketReturn = calculateMarketReturn(); 
+        if (beta.isPresent()) {
+            expectedReturnRate = riskFreeRate + beta.get() * (marketReturn - riskFreeRate);
+        }
         return expectedReturnRate;
     }
-    
-    
+    private double calculateRiskFreeRate() {        
+        return 0.02; //TODO replace with actual risk free rate
+    }
+    private double calculateMarketReturn() {        
+        return 0.07; //TODO replace with actual market return
+    }
 
     // fxn to get beta from Newton API 
     public Optional<Double> getBetaFromNewtonApi(String ticker) {
