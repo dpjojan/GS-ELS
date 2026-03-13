0f96d570b8dfb3ba4a3b3096dae5b0ba04c95aef
ambers7 <ambrshen@gmail.com>
2026-03-12 20:55:17 -0400

    Hardcoded list of funds

--- mutual-fund-backend/src/main/java/com/mutualfund/mutual_fund_backend/MutualFundController.java
+++ mutual-fund-backend/src/main/java/com/mutualfund/mutual_fund_backend/MutualFundController.java
@@ -22,24 +22,22 @@ public class MutualFundController {
     @Autowired // SpringBoot creates an instance of MutualFundService to use
     private MutualFundService mutualFundService;
 
-    // TODO: hardcode list of mutual funds
     // when frontend requests mutual funds, return this list of hardcoded funds
     @GetMapping("/funds")
     public ArrayList<MutualFund> getFunds() {
         ArrayList<MutualFund> fundsList = new ArrayList<>();
-        fundsList.add(new MutualFund("ticker1", "fund1"));
-        fundsList.add(new MutualFund("ticker2", "fund2"));
-        fundsList.add(new MutualFund("ticker3", "fund3"));
-        fundsList.add(new MutualFund("ticker3", "fund4"));
+        fundsList.add(new MutualFund("VBTLX", "Vanguard Total Bond Market Index Fund"));
+        fundsList.add(new MutualFund("SWAGX", "Schwab US Aggregate Bond Index Fund"));
+        fundsList.add(new MutualFund("VFIAX", "Vanguard 500 Index Fund"));
+        fundsList.add(new MutualFund("FXAIX", "Fidelity 500 Index Fund"));
+        fundsList.add(new MutualFund("VSMAX", "Vanguard Small Cap Index Fund"));
+        fundsList.add(new MutualFund("SWLGX", "Schwab Large Cap Growth Fund"));
         return fundsList;
     }
     
-    /*TODO: GET future value of investment amount (call fxn in MutualFundService and pass on to frontend)
-    - depends on MutualFund, MutualFundService being completed
-     */
     // when frontend requests future val of a ticker given principal and length of time, return the expected future value
     @GetMapping("/futureVal")
-    public double getFutureVal( //TODO change to Map<String,Object>?
+    public double getFutureVal( //change to Map<String,Object> if want to send additional information back
         @RequestParam String ticker,
         @RequestParam double principal,
         @RequestParam int years){
