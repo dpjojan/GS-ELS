3463eb75514ff0a76e8704f4c4f41e7dd640f71b
dpjojan <dpjojan@gmail.com>
2026-04-06 21:57:35 -0400

    add alpha, beta1, beta2 fields to MutualFund entity for Fama-French bond model

--- mutual-fund-backend/src/main/java/com/mutualfund/mutual_fund_backend/MutualFund.java
+++ mutual-fund-backend/src/main/java/com/mutualfund/mutual_fund_backend/MutualFund.java
@@ -12,6 +12,9 @@ public class MutualFund {
     private String ticker;
     private String name;
     private String risk;
+    private Double alpha;
+    private Double beta1;
+    private Double beta2;
 
     public MutualFund() {}
 
@@ -31,5 +34,29 @@ public class MutualFund {
 
     public String getRisk() {
         return risk;
+    }
+
+    public Double getAlpha() {
+        return alpha;
+    }
+
+    public Double getBeta1() {
+        return beta1;
+    }
+
+    public Double getBeta2() {
+        return beta2;
+    }
+
+    public void setAlpha(Double alpha) {
+        this.alpha = alpha;
+    }
+
+    public void setBeta1(Double beta1) {
+        this.beta1 = beta1;
+    }
+
+    public void setBeta2(Double beta2) {
+        this.beta2 = beta2;
     }
 }
